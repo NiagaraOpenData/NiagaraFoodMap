fd56fe865afedbb5aec78e7ad8e93c97dd11875b
Michael Mottola <mikemottola@gmail.com>
2016-03-06 00:20:12 -0500

    Update homepage page map and map marker popup

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -4,7 +4,7 @@ class VisitorsController < ApplicationController
     @foodbanks = Gmaps4rails.build_markers(Foodbank.all) do |foodbank, marker|
       marker.lat foodbank.latitude
       marker.lng foodbank.longitude
-      marker.infowindow foodbank.info['Name']
+      marker.infowindow "<h4>#{foodbank.info['Name']}</h4> <p><h5>Description</h5><p>#{foodbank.info['Description (Service)']}</p> <h5>Hours</h5><p>#{foodbank.info['Hours']}</p> <h5>Address</h5><p>#{foodbank.info['Address']}</p>"
     end
   end
 end
